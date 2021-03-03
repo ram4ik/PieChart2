371ab03a36e7b37fedb8c7e617b7c7ab0ac01885
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-03 15:26:59 +0200

    Initial Commit

---
+++ PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by ramil on 03.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
